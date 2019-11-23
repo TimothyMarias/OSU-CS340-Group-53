36757e18486e4f2640ee520f35d8e9cd0cf9c9fd
Kurt Kaiser <kurtbkaiser@gmail.com>
2019-11-23 16:00:41 -0500

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,12 +1,13 @@
 <html>
 <h1>Update {{movie.title}} {{movie.release_year}}:</h1>
 <form id="update-movie" action="/movie" method="post">
-    Title: <input type="text" name="title" value="{{movie.title}}"><br>
-    Box Office: <input type="text" name="box_office" value="{{movie.box_office}}"><br>
-		Year: <input type="number" name="release_year" value="{{movie.release_year}}"><br>
-		Genre: <select name="genre" id="genre-selector"> 
-        {{#each planets}}
-            <option value="{{id}}">{{name}}</option>
+	Title: <input type="text" name="title" value="{{movie.title}}"><br>
+	Box Office: <input type="number" name="box_office" value="{{movie.box_office}}"><br>
+	Year: <input type="number" name="release_year" value="{{movie.release_year}}"><br>
+	Director: <input type="text" name="director" value="{{movie.director}}"><br>
+	Top Billed Actor: <input type="text" name="actor" value="{{movie.actor}}"><br>
+	Genre: <input type="text" name="box_office" value="{{movie.genre}}"><br>
+        <option value="{{id}}">{{name}}</option>
         {{/each}}
     </select><br>
 </form>
