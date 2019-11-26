040820554e422264a6628043bb3c70fed77a6684
Kurt Kaiser <kurtbkaiser@gmail.com>
2019-11-25 20:09:47 -0500

    Update film_dml.sql

--- film_dml.sql
+++ film_dml.sql
@@ -10,11 +10,11 @@ INSERT INTO people_film (people_id, name, role, movie)
 VALUES (NULL, :name_input, :role_input, :title_input);
 
 -- Update --- 
-UPDATE film_movie SET title = :title_input, box_office = :box_office_input 
-WHERE film_movie.film_id = :film_id_input; 
+UPDATE film SET title = :title_input, box_office = :box_office_input 
+WHERE film.film_id = :film_id_input; 
 
-UPDATE people_film SET role = :role_input, movie = :title_input
-WHERE people_film.people_id = :people_id_input; 
+UPDATE people SET role = :role_input, movie = :title_input
+WHERE people.people_id = :people_id_input; 
 
 -- Search ---
 SELECT * FROM film_movie WHERE title LIKE :title_input;
@@ -22,6 +22,6 @@ SELECT * FROM people_film WHERE name LIKE :name_input;
 SELECT * FROM film_movie WHERE film_id = :film_id_input;
 
 -- DELETE --
-DELETE FROM film_movie WHERE film_movie.box_office = :box_office_input;
-DELETE FROM people_film WHERE people_film.people_id = :people_film_id;
-DELETE FROM academy_award_film WHERE aaid = :aaid_input;
+DELETE FROM film WHERE film_movie.box_office = :box_office_input;
+DELETE FROM people WHERE people.people_id = :people_id;
+DELETE FROM award_film WHERE aaid = :aaid_input;
