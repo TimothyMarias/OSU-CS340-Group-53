92968f8f90d171ac947515164c53b9cfa1d3590b
Kurt Kaiser <kurtbkaiser@gmail.com>
2019-11-25 19:48:36 -0500

    Update film_ddq.sql

--- film_ddq.sql
+++ film_ddq.sql
@@ -2,8 +2,6 @@
 
 -- Table structure for table film
 DROP TABLE IF EXISTS `film`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 
 CREATE TABLE `film` (
   `film_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -11,58 +9,45 @@ CREATE TABLE `film` (
   `box_office` bigint(20) DEFAULT NULL,
   `genre` varchar(255) DEFAULT NULL,
 	`release_year` int(4) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`film_id`),
   UNIQUE KEY `title` (`title`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `film`
 --
 
 LOCK TABLES `film` WRITE;
-/*!40000 ALTER TABLE `film` DISABLE KEYS */;
 INSERT INTO `film` VALUES (1,'	Star Wars: The Force Awakens',2068223624,'Science-Fiction', 2015),(2,'Jurassic World', 1671713208,'Science-Fiction', 2015),(3,'	Furious 7',1516045911,'Action', 2015), (4,'Bohemian Rhapsody',903655259,'Drama', 2015);
-/*!40000 ALTER TABLE `film` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `award`
 --
 
 DROP TABLE IF EXISTS `award`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `award` (
   `award_id` int(11) NOT NULL AUTO_INCREMENT,
   `movies` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`award_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `award`
 --
 
 LOCK TABLES `award` WRITE;
-/*!40000 ALTER TABLE `award` DISABLE KEYS */;
 INSERT INTO `award` VALUES (1,'Best Picture'),(2,'Best Director'),(3,'Best Actor'),(4,'Best Actress');
-/*!40000 ALTER TABLE `award` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `people`
 --
 
 DROP TABLE IF EXISTS `people`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `people` (
   `people_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `role` varchar(255) DEFAULT NULL,
   `movie` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`people_id`),
   KEY `movie` (`movie`),
   CONSTRAINT `people_ibfk_1` FOREIGN KEY (`movie`) REFERENCES `film` (`title`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
