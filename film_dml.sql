ff82662176b6035c152b0ea131d3993d65201039
Tim <mariast@oregonstate.edu>
2019-12-02 09:54:22 -0800

    updated dml

--- film_dml.sql
+++ film_dml.sql
@@ -2,26 +2,26 @@
 
 -- Insert --
   -- Film Info - the actual blockbuster movie
-INSERT INTO film_movie (NULL, title, box_office, genre, release_year) 
+INSERT INTO 'film_movie' ('film_id', 'title', 'box_office', 'genre', 'release_year') 
 VALUES (NULL, :title_input, :box_office_input, :genre_input, :year_input);
 
 -- People - actors, actresses and directors
-INSERT INTO people_film (people_id, name, role, movie) 
+INSERT INTO 'people_film' ('people_id', 'name', 'role', 'movie') 
 VALUES (NULL, :name_input, :role_input, :title_input);
 
 -- Update --- 
-UPDATE film SET title = :title_input, box_office = :box_office_input 
-WHERE film.film_id = :film_id_input; 
+UPDATE 'film' SET 'title' = :title_input, box_office = :box_office_input 
+WHERE 'film.film_id' = :film_id_input; 
 
-UPDATE people SET role = :role_input, movie = :title_input
-WHERE people.people_id = :people_id_input; 
+UPDATE 'people' SET 'role' = :role_input, movie = :title_input
+WHERE 'people.people_id' = :people_id_input; 
 
 -- Search ---
-SELECT * FROM film_movie WHERE title LIKE :title_input;
-SELECT * FROM people_film WHERE name LIKE :name_input; 
-SELECT * FROM film_movie WHERE film_id = :film_id_input;
+SELECT * FROM 'film_movie' WHERE 'title' LIKE :title_input;
+SELECT * FROM 'people_film' WHERE 'name' LIKE :name_input; 
+SELECT * FROM 'film_movie' WHERE 'film_id' = :film_id_input;
 
 -- DELETE --
-DELETE FROM film WHERE film_movie.box_office = :box_office_input;
-DELETE FROM people WHERE people.people_id = :people_id;
-DELETE FROM award_film WHERE aaid = :aaid_input;
+DELETE FROM 'film' WHERE 'film_movie.box_office' = :box_office_input;
+DELETE FROM 'people' WHERE 'people.people_id' = :people_id;
+DELETE FROM 'award_film' WHERE 'aaid' = :aaid_input;
